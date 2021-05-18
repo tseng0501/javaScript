fe02e7f91a1037f299450d1f4dade17949e68d2e
tseng0501 <a0908802569@gmail.com>
2021-05-18 15:17:52 +0800

    完成backEndSetting。html

--- js/text.js
+++ js/text.js
@@ -5,7 +5,7 @@ $(function () {
         type: 'GET',
         success: function (result) {
             if (result.status) {
-                console.log(result, "result")
+                console.log(result,"result")
                 $("#form").dxForm({
                     formData: result,
                     items: [{
@@ -23,7 +23,7 @@ $(function () {
                     }, {
                         itemType: "group",
                         cssClass: "second-group",
-                        items: [{
+                        items: [ {
                             colCount: 3,
                             itemType: "group",
                             caption: "結構名稱",
@@ -52,60 +52,62 @@ $(function () {
             DevExpress.ui.notify("連線錯誤", "error", 3000);
         }
     });
-    $.ajax({
-        url: url + "/backEndSetting/getData",
-        type: 'GET',
-        dataType: 'json',
-        success: function (result) {
-            if (result.status) {
-                console.log(result, "====")
-                $("#server").dxSelectBox({
-                    displayExpr: "name",
-                    dataSource: result.data,
-                    value: result.data[0],
-                    onValueChanged: function (data) {
-                        form2.option("formData", data.value);
-                    }
-                });
-                var form2 = $("#form2").dxForm({
-                    formData: result.data[0],
-                    readOnly: true,
-                    labelLocation: "top",
-                    showColonAfterLabel: true,
-                    colCount: 3,
-                    items: [{
-                        dataField: "ip",
-                        label: {
-                            text: "ip"
+     $.ajax({
+            url: url + "/backEndSetting/getData",
+            type: 'GET',
+            dataType: 'json',
+            success: function (result) {
+                if (result.status) {
+                    console.log(result,"====")
+                    $("#server").dxSelectBox({
+                        displayExpr: "name",
+                        dataSource: result.data,
+                        value: result[0],
+                        onValueChanged: function(data) {
+                           form2.option("formData", data.value);
                         }
-                    }, {
-                        dataField: "port",
-                        label: {
-                            text: "port"
-                        }
-                    }]
-                }).dxForm("instance");
-                DevExpress.ui.notify("獲取成功", "success", 3000);
-            } else {
-                DevExpress.ui.notify("獲取失敗", "error", 3000);
+                    });
+                    var form2 = $("#form2").dxForm({
+                        formData:result,
+                        readOnly: true,
+                        labelLocation: "top",
+                        showColonAfterLabel: true,
+                        items:[{
+                            colCount: 1,
+                            itemType: "group",
+                            caption: "結構名稱",
+                            items: [{
+                                dataField: "result.ip",
+                                label: {
+                                    text: "ip"
+                                }
+                            }, {
+                                dataField: "result.port",
+                                label: {
+                                    text: "port"
+                                }
+                            }]
+                        }]
+                    }).dxForm("instance");
+                    DevExpress.ui.notify("獲取成功", "success", 3000);
+                } else {
+                    DevExpress.ui.notify("獲取失敗", "error", 3000);
+                }
+            },
+            error: function (result) {
+                DevExpress.ui.notify("連線錯誤", "error", 3000);
             }
-        },
-        error: function (result) {
-            DevExpress.ui.notify("連線錯誤", "error", 3000);
-        }
-    });
-
+        });
+   
     $("#send").dxButton({
         stylingMode: "contained",
         text: "確認",
         type: "success",
         width: 120,
-        onClick: function () {
+        onClick: function() {
             $('#form').dxForm('instance').validate();
-            $('#form2').dxForm('instance').validate();
-            var data = $('#form').dxForm('instance').option('formData');
-            data.serevr = $('#form2').dxForm('instance').option('formData');
-            console.log(data, "data")
+            var data = $('#form').dxForm('instance').option('formData');  
+            console.log(data,"data")
             $.ajax({
                 url: url + '/putForm',
                 type: 'PUT',
@@ -156,7 +158,7 @@ $(function () {
                 dataField: "modeID",
                 caption: "模式",
                 lookup: {
-                    dataSource: function (options) {
+                    dataSource: function(options) {
                         return {
                             store: new DevExpress.data.CustomStore({
                                 key: "id",
@@ -164,7 +166,7 @@ $(function () {
                                 load: function () {
                                     return sendRequest(url + "/getMode");
                                 }
-
+    
                             }),
                             filter: options.data ? ["nameID", "=", options.data.modeID] : null
                         };
@@ -245,7 +247,7 @@ $(function () {
                             dataField: "modeID",
                             caption: "模式",
                             lookup: {
-                                dataSource: function (options) {
+                                dataSource: function(options) {
                                     return {
                                         store: new DevExpress.data.CustomStore({
                                             key: "id",
@@ -253,7 +255,7 @@ $(function () {
                                             load: function () {
                                                 return sendRequest(url + "/getSecondMode");
                                             }
-
+                
                                         }),
                                         filter: options.data ? ["nameID", "=", options.data.modeID] : null
                                     };
@@ -272,7 +274,7 @@ $(function () {
                                 return options.data.enable == false;
                             }
                         }],
-
+                       
                     }).appendTo(container);
             }
         }
@@ -294,7 +296,7 @@ $(function () {
                 } else if (method == "PUT") {
                     DevExpress.ui.notify("已更新", "success", 3000);
                 }
-                d.resolve(method === "GET" ? result.data : result);
+            d.resolve(method === "GET" ? result.data : result);
             } else {
                 DevExpress.ui.notify("執行失敗", "error", 3000);
                 d.reject(result.message);
@@ -308,3 +310,44 @@ $(function () {
     }
 });
 
+var companies = [{
+    "ID": 1,
+    "Name": "Super Mart of the West",
+    "Address": "702 SW 8th Street",
+    "City": "Bentonville",
+    "State": "Arkansas",
+    "ZipCode": 72716,
+    "Phone": "(800) 555-2797",
+    "Fax": "(800) 555-2171",
+    "Website": "http://www.nowebsitesupermart.com"
+}, {
+    "ID": 2,
+    "Name": "Electronics Depot",
+    "Address": "2455 Paces Ferry Road NW",
+    "City": "Atlanta",
+    "State": "Georgia",
+    "ZipCode": 30339,
+    "Phone": "(800) 595-3232",
+    "Fax": "(800) 595-3231",
+    "Website": "http://www.nowebsitedepot.com"
+}, {
+    "ID": 3,
+    "Name": "K&S Music",
+    "Address": "1000 Nicllet Mall",
+    "City": "Minneapolis",
+    "State": "Minnesota",
+    "ZipCode": 55403,
+    "Phone": "(612) 304-6073",
+    "Fax": "(612) 304-6074",
+    "Website": "http://www.nowebsitemusic.com"
+}, {
+    "ID": 4,
+    "Name": "Tom's Club",
+    "Address": "999 Lake Drive",
+    "City": "Issaquah",
+    "State": "Washington",
+    "ZipCode": 98027,
+    "Phone": "(800) 955-2292",
+    "Fax": "(800) 955-2293",
+    "Website": "http://www.nowebsitetomsclub.com"
+}];
