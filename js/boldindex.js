031e0d15c8998b7ad90ca8d6349bf45e4cc781fa
tseng0501 <a0908802569@gmail.com>
2021-07-27 09:49:52 +0800

    刪除檔案

--- js/boldindex.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $("#designer").boldReportViewer({
-        // serviceUrl: '/demos/services/api/ReportDesignerWebApi',
-        serviceUrl: "https://demos.boldreports.com/services/api/ReportingAPI",
-        reportServiceUrl: "https://demos.boldreports.com/services/api/ReportViewer",
-        reportPath: '~/Resources/docs/sales-order-detail.rdl'
-
-    });
-});
